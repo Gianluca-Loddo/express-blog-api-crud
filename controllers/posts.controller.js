4c66cfd76421e9a50042c551f02486c3fe506cbe
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-16 18:31:33 +0200

    implement store route

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -20,7 +20,7 @@ const index = (req, res) => {
 const show = (req, res) => {
     const id = parseInt(req.params.id);
     const post = postsDATA.find(item => item.id === id);
-     if (!post) {
+    if (!post) {
         res.status(404) //404 significato: "pagina non trovata"
         return res.json({
             error: "Not Found",
@@ -31,7 +31,28 @@ const show = (req, res) => {
 };
 
 const store = (req, res) => {
-    res.send("Creazione di un nuovo post")
+    //creiamo un nuovo ID incrementando l'ultimo ID disponibile
+    const newID = postsDATA[postsDATA.lenght - 1].id + 1;
+
+    //creiamo un nuovo oggetto POST
+    const newPOST = {
+        id: newID,
+        titolo: req.body.titolo,
+        contenuto: req.body.contenuto,
+        immagine: req.body.immagine,
+        tags: req.body.tags
+    }
+    //aggiungiamo il nuovo posto (PUSH in ultima posizione della stringa di oggetti (POST) di postsDAT)
+    postsDATA.push(newPOST);
+
+    //controlliamo che il nuovo post sia stato aggiunto
+    console.log(postsDATA);
+
+    //res.send("Creazione di un nuovo post") --> commentato in quanto ora non ci serve più, fa parte della prima parte dell'esercizio
+
+    //restituiamo lo status corretto e il post appena aggiunto
+    res.status(201); // Il codice di stato 201 Created va oltre il semplice messaggio di successo e fornisce ulteriori informazioni. Indica che non solo la richiesta è andata a buon fine, ma che è stata creata una nuova risorsa sul server.
+    res.json(newPOST); //restituisce il nuovo post in formato json 
 };
 
 const update = (req, res) => {
--- main.js
+++ main.js
@@ -5,15 +5,19 @@ const port = 3000; // porta di dialogo per il server
 //importiamo le rotte (group routing ./ROUTER/POSTS )
 const postsROUTER = require("./ROUTER/posts");
 
+//registriamo in express il body-PARSER in formato json (PER L'APPLICATION JSON)
+app.use(express.json());
+
 //diciamo ad express dobbiamo usare le rotte 
 app.use("/posts", postsROUTER);
+
+// impostazione asset statici (per poter visionare le immagini nella cartella /PUBLIC/images )
+app.use(express.static("PUBLIC")); 
 
 
 app.listen(port, () => { //ci mettiamo in ascolto sulla porta 3000
     console.log("This server is listening on port" + port)
 })
-
-app.use(express.static("PUBLIC")); // impostazione asset statici (per poter visionare le immagini nella cartella /PUBLIC/images )
 
 
 
